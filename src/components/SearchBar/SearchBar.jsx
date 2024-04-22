7e897032aac7bec57daca124495c998a5c32a0f2
Anton <antonklim6@gmail.com>
2024-04-22 13:10:16 +0200

    upd error

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import { Formik, Form, Field } from "formik";
 import { IoSearch } from "react-icons/io5";
 import css from "./SearchBar.module.css";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function SearchBar ({ onSearch }) {
     return (
@@ -8,6 +9,10 @@ export default function SearchBar ({ onSearch }) {
             <Formik
                 initialValues={{ query: "" }}
                 onSubmit={(values, actions) => {
+                    if(values.query.trim() === "") {
+                        toast.error("Sorry, input is empty. Please try again!");
+                        return;
+                    }
                     onSearch(values.query)
                     actions.resetForm()
                 }}
@@ -17,6 +22,7 @@ export default function SearchBar ({ onSearch }) {
                     <button className={css.button} type="submit"><IoSearch className={css.icon} size={15} />Search</button>
                 </Form>
             </Formik>
+            <Toaster position="top-right" reverseOrder={false} />
         </header>
     )
 }
